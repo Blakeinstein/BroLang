458340ef38c5c0e9da9cd0dee827232907f3a4f1
Krutik Pandya <112588253+kpandya8@users.noreply.github.com>
2023-04-21 12:22:55 -0700

    Implementation of block and print statements.

--- src/parser/visitor.py
+++ src/parser/visitor.py
@@ -7,16 +7,19 @@ from compiler.BroLangVisitor import BroLangVisitor
 class Visitor(BroLangVisitor):
   data_map: dict[str, str | float] = dict()
 
+ 
   def assert_env(self, key: str):
     if key not in self.data_map:
       raise Exception(
         f"My brother in christ, variable <{id}> not found, did you forget to declare it?"
       )
   
+ 
   def visitProg(self, ctx:BroLangParser.ProgContext):
     self.visitChildren(ctx)
     return 0
 
+ 
   def visitDeclaration(self, ctx:BroLangParser.DeclarationContext):
     id = self.visit(ctx.identifier())
     val = 0.0
@@ -24,6 +27,7 @@ class Visitor(BroLangVisitor):
       val = self.visit(ctx.data())
     self.data_map[id] = val
 
+ 
   def visitAssignment(self, ctx:BroLangParser.AssignmentContext):
     id = self.visit(ctx.identifier())
     val = self.visit(ctx.data())
@@ -31,3 +35,59 @@ class Visitor(BroLangVisitor):
     self.assert_env(id)
 
     self.data_map[id] = val
+
+ 
+  def visitIfthenelse(self, ctx:BroLangParser.IfthenelseContext):
+    if self.visit(ctx.expression()):
+      self.visit(ctx.true_block)
+    else:
+      self.visit(ctx.false_block)
+
+  
+  def visitWhileBlock(self, ctx:BroLangParser.WhileBlockContext):
+    while self.visit(ctx.expression()):
+      self.visit(ctx.block())
+
+  
+  def visitForBlock(self, ctx:BroLangParser.ForBlockContext):
+    self.visit(ctx.pre)
+    
+    while self.visit(ctx.expression()):
+      self.visit(ctx.loop)
+      self.visit(ctx.post)
+
+ 
+  def visitForRange(self, ctx:BroLangParser.ForRangeContext):
+    id = self.visit(ctx.identifier())
+    old_val = None
+    if id in self.data_map:
+      old_val = self.data_map[id]
+    
+    i = self.visit(ctx.start)
+    end = self.visit(ctx.end)
+
+    while i < end:
+      self.data_map[id] = i
+      self.visit(ctx.block())
+      i += 1
+    
+    if old_val:
+      self.data_map[id] = old_val
+    else:
+      self.data_map.pop(id)
+
+  
+  def visitPrint(self, ctx:BroLangParser.PrintContext):
+    print(*self.visit(ctx.vals(), end=""))
+
+
+  def visitPrintln(self, ctx:BroLangParser.PrintlnContext):
+    print(*self.visit(ctx.vals()))
+
+  
+  def visitVals(self, ctx:BroLangParser.ValsContext):
+    return [self.visit(child) for child in ctx.data_terms]
+
+
+  def visitData(self, ctx:BroLangParser.DataContext):
+    return self.visitChildren(ctx)
